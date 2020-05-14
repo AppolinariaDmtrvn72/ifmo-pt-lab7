73d7d1cb36d7eeae6550f32facb47724e0d219a9
AppolinariaDmtrvn72 <poly1402@yandex.ru>
2020-05-14 23:04:56 +0300

    done lab

---
+++ build/tmp/compileGroovy/groovy-java-stubs/ifmo/pt/lab5/groovy/FirstClass.java
@@ -0,0 +1,30 @@
+package ifmo.pt.lab5.groovy;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class FirstClass
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+;
+public FirstClass
+(java.lang.Integer id, java.lang.String title, java.lang.Boolean isBlocked, java.lang.Float rating) {}
+public FirstClass
+() {}
+@groovy.transform.Generated() @groovy.transform.Internal() public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+@groovy.transform.Generated() @groovy.transform.Internal() public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  void setId(java.lang.Integer value) { }
+public  java.lang.String getTitle() { return (java.lang.String)null;}
+public  void setTitle(java.lang.String value) { }
+public  java.lang.Boolean getIsBlocked() { return (java.lang.Boolean)null;}
+public  void setIsBlocked(java.lang.Boolean value) { }
+public  java.lang.Float getRating() { return (java.lang.Float)null;}
+public  void setRating(java.lang.Float value) { }
+public  java.lang.Object getBinding() { return null;}
+public  void setBinding(java.lang.Object value) { }
+public  java.lang.Integer getId() { return (java.lang.Integer)null;}
+}
---
+++ build/tmp/compileGroovy/groovy-java-stubs/ifmo/pt/lab5/groovy/MainClass.java
@@ -0,0 +1,25 @@
+package ifmo.pt.lab5.groovy;
+
+import java.time.*;
+import java.util.*;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class MainClass
+  extends groovy.lang.Script {
+;
+public MainClass
+() {}
+public MainClass
+(groovy.lang.Binding context) {
+super ();
+}
+public static  void main(java.lang.String... args) { }
+public  java.lang.Object run() { return null;}
+public  int methodReturnsInt(java.lang.Integer a) { return (int)0;}
+public  java.lang.Integer methodReturnsInteger(java.lang.Integer a) { return (java.lang.Integer)null;}
+}
